d9f0145c79a5c4180c0e4cbd28fff8752847c597
sulekhs <97458261+sulekhs@users.noreply.github.com>
2022-06-02 19:22:21 +0530

    Update default.ts

--- config/default.ts
+++ config/default.ts
@@ -1,9 +1,9 @@
 export default {
     port: 7100,
-    dbUri: "mongodb+srv://nodetypescript:nodetypescript@cluster0.nz7c0.mongodb.net/nodetypescript_practice?retryWrites=true&w=majority",
+    dbUri: "mongodb+srv://username:password@cluster0.nz7c0.mongodb.net/databasename?retryWrites=true&w=majority",
     saltWorkFactor: 10,
-    publicKey: `sulekhhdef`,
-    privateKey: `sulekhshende`,
+    publicKey: ``,
+    privateKey: ``,
     accessTokenTtl: '15m',
     refreshTokenTtl: "1y"
 }
